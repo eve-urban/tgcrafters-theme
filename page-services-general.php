0976a9a1ee8af19c759a7949f45b3d45c3d19615
Eve <ewa.urbanowska@nottingham.ac.uk>
2019-10-23 17:41:37 +0100

    Add page template for general services

---
+++ page-services-general.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: General service 
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+
+/*
+// Enqueue vertical tabs.
+function enqueue_scripts_home_page() {
+
+
+    wp_enqueue_script(
+        'vertical-tabs',
+        get_stylesheet_directory_uri() . '/js/vertical-tabs.js',
+        array(),
+        wp_get_theme('tgcrafters')->get('Version'),
+        true
+    );
+
+
+
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts_home_page', 100);
+
+*/
+
+Timber::render( 'page_templates/services_general.twig', $context );
