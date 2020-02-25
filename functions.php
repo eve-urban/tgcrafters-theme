5c04716ad48d438ea0ae8a22c764015205639613
Eve <ewa.urbanowska@nottingham.ac.uk>
2020-02-25 14:21:17 +0000

    Remove second font awesome reference

--- functions.php
+++ functions.php
@@ -134,3 +134,12 @@ function register_theme_features() {
 	);
 }
 add_action( 'init', 'register_theme_features' );
+
+
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
